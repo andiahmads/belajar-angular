24d8dd95a3ffef659e56424ebb48872835ad1a75
andiahmads <andi.fivesco@gmail.com>
2022-06-29 14:32:08 +0700

    module component

---
+++ src/app/testing.component.ts
@@ -0,0 +1,14 @@
+import { style } from '@angular/animations';
+import { Component} from '@angular/core';
+
+
+@Component({    
+    selector: 'app-test',
+    template:'<h1>testing component</h1>',
+    styles: ['h1 { color:blue }' ]
+
+})
+
+export class TestComponent {
+    
+}
